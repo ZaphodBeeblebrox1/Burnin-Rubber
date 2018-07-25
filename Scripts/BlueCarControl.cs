49e04048b89fe47223d77c48ce3bab170d4793ec
ZaphodBeeblebrox1 <40953504+ZaphodBeeblebrox1@users.noreply.github.com>
2018-07-25 17:59:21 -0400

    Add files via upload

---
+++ Scripts/BlueCarControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class BlueCarControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+
+            float h = CrossPlatformInputManager.GetAxis("Horizontal1");
+            float v = CrossPlatformInputManager.GetAxis("Vertical1");
+
+
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(h, v, v, handbrake);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Scripts/WinMessage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinMessage : MonoBehaviour {
+    public Text winText;
+	// Use this for initialization
+	void Start () {
+        winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player1")
+        {
+            print("BLUE!!");
+            winText.text = "Blue Wins!!";
+        }
+        else
+        {
+            winText.text = "Green Wins!!";
+        }
+
+    }
+
+}
